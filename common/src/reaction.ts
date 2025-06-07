4cd835addcff6a595df38159ae36615b9a59ebe1
James Koppel <jamesbkoppel@yahoo.com>
2025-06-07 03:17:03 -0700

    Removing all but Mani

--- common/src/reaction.ts
+++
@@ -1,8 +0,0 @@
-import { Row } from './supabase/utils'
-
-export type Reaction = Row<'user_reactions'>
-
-export type ReactionContentTypes = 'contract' | 'comment' | 'post'
-export type ReactionType = 'like' | 'dislike'
-
-// export type ReactionTypes = 'like'
--- web/hooks/use-is-eligible-for-loans.ts
+++
@@ -1,9 +0,0 @@
-import { useCurrentPortfolio } from 'web/hooks/use-portfolio-history'
-import { isUserEligibleForLoan } from 'common/loans'
-
-export const useIsEligibleForLoans = (userId: string) => {
-  const latestPortfolio = useCurrentPortfolio(userId)
-  if (!latestPortfolio) return { latestPortfolio, isEligible: false }
-  const isEligible = isUserEligibleForLoan({ ...latestPortfolio, userId })
-  return { latestPortfolio, isEligible }
-}
